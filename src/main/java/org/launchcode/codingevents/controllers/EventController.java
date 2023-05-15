0434c1369c7dd0a201e7697d4ebefcf6e5c2ccdb
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-05-15 11:36:04 -0500

    set up category links and display of category events

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,6 +4,7 @@ import org.launchcode.codingevents.data.EventCategoryRepository;
 import org.launchcode.codingevents.data.EventRepository;
 import org.launchcode.codingevents.models.Event;
 
+import org.launchcode.codingevents.models.EventCategory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("events")
@@ -23,10 +25,27 @@ public class EventController {
     private EventCategoryRepository eventCategoryRepository;
 
     @GetMapping
-    public String displayAllEvents(Model model) {
+    public String displayAllEvents(Model model, @RequestParam(required = false) Integer categoryId) {
+
+        if(categoryId == null){
         model.addAttribute("title", "All Events");
         model.addAttribute("events", eventRepository.findAll());
         return "events/index";
+        } else {
+            Optional<EventCategory> result = eventCategoryRepository.findById(categoryId);
+
+            if(result.isEmpty()){
+                model.addAttribute("title", "Invalid Category ID: " + categoryId);
+            } else {
+                EventCategory category = result.get();
+                model.addAttribute("title", "Events in category: " + category.getName());
+                model.addAttribute("events", category.getEvents());
+
+            }
+
+
+            return "events/index";
+        }
     }
 
     @GetMapping("create")
